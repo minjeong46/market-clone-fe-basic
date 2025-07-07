1807ea48985548f9f51b5ee47edd4e179c0d43db
minjeong46 <eyou46@naver.com>
2025-07-07 17:26:21 +0900

    글 작성, FormData 보내기

--- main.py
+++ main.py
@@ -1,45 +1,10 @@
 from fastapi import FastAPI,UploadFile,Form
-from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
-from typing import Annotated
 from pydantic import BaseModel
-import sqlite3
+from typing import Annotated
 
-# con = sqlite3.connect('db.db', check_same_thread=False)
-# cur = con.cursor()
 
 app = FastAPI()
-
-# @app.post("/items")
-# async def create_item(image:UploadFile, 
-#                 title:Annotated[str,Form()], 
-#                 price:Annotated[int,Form()],
-#                 description:Annotated[str,Form()],
-#                 place:Annotated[str,Form()],
-#                 insertAt:Annotated[int,Form()]):
-#     # print(image, title, price,description,place)
-    
-#     image_bytes = await image.read()
-#     cur.execute(f"""
-#                 INSERT INTO items(title, image, price, description, place, insertAt)
-#                 VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}',{insertAt})
-#                 """)
-#     con.commit()
-    
-#     return'200'
-
-# @app.get("/items")
-# async def get_items() :
-#     # 컬럼명도 같이 가져옴
-#     con.row_factory = sqlite3.Row
-    
-#     rows = cur.execute(f"""
-#                        SELECT * from items;
-#                        """).fetchall()
-    
-#     return JSONResponse(rows)
-
-
 
 class Msg(BaseModel):
     id: str
@@ -55,5 +20,15 @@ def create_msg(msg:Msg) :
 @app.get("/chat")
 def read_msg() :
     return msgs;
+
+@app.post("/item")
+def create_item(image:UploadFile,
+                title:Annotated[str,Form()], 
+                price:Annotated[int,Form()], 
+                description:Annotated[str,Form()], 
+                place:Annotated[str,Form()]):
+    print(image,title,price,description,place)
+    return "200"
+
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/write.js
+++ static/write.js
@@ -2,18 +2,10 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (e) => {
     e.preventDefault();
-    const body = new FormData(form);
-    body.append("insertAt", new Date().getTime());
-    try {
-        const res = await fetch("/items", {
-            method: "POST",
-            body,
-        });
-        const data = await res.json();
-        if (data === "200") window.location.pathname = "/";
-    } catch (e) {
-        console.error("이미지 업로드에 실패");
-    }
+    const res = await fetch("/item", {
+        method: "POST",
+        body: new FormData(form),
+    })
 };
 
 form.addEventListener("submit", handleSubmitForm);
