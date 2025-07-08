96bad0aa6e96b9e9396953c9cd2226214ddb0eb3
minjeong46 <eyou46@naver.com>
2025-07-08 13:35:57 +0900

    feat: 글쓰기 및 메인 글 GET 가져오기
    - SQLite 와 연동하여 글 작성 및 글 가져오기
    - 그런 글 POST, GET 를 python 을 이용하여 DB에 저장 및 불러오도록 구현
    - 프론트엔드 구현

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI,UploadFile,Form
+from fastapi import FastAPI,UploadFile,Form,Response
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from typing import Annotated
 
+import sqlite3
+
+con = sqlite3.connect('db.db', check_same_thread=False)
+cur = con.cursor()
 
 app = FastAPI()
 
@@ -22,13 +28,38 @@ def read_msg() :
     return msgs;
 
 @app.post("/item")
-def create_item(image:UploadFile,
+async def create_item(image:UploadFile,
                 title:Annotated[str,Form()], 
                 price:Annotated[int,Form()], 
                 description:Annotated[str,Form()], 
-                place:Annotated[str,Form()]):
-    print(image,title,price,description,place)
-    return "200"
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()]
+                ):
+                
+    image_bytes = await image.read() # 이미지를 읽는다
+    cur.execute(f"""
+                INSERT INTO items(title, image, price, description, place, insertAt)
+                VALUES ('{title}', '{image_bytes.hex()}',{price}, '{description}', '{place}', {insertAt})
+                """) #이미지를 hex 16진법으로
+    con.commit()
+    return '200'
 
+@app.get("/item")
+async def read_item():
+    con.row_factory = sqlite3.Row # 컬럼명도 같이 가져옴
+    cur = con.cursor() # con의 위치 업데이트
+    rows = cur.execute(f"""
+                       SELECT * FROM items
+                       """).fetchall()
+    # return JSONResponse(rows) -> list 형태가 감
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows)) #json 으로 바꿔서 encoder -> response 응답
+
+@app.get("/images/{item_id}")
+def read_image(item_id):
+    cur = con.cursor()
+    image_bytes = cur.execute(f"""
+                              SELECT image FROM items WHERE id={item_id}
+                              """).fetchone()[0]
+    return Response(content=bytes.fromhex(image_bytes))
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
--- static/write.js
+++ static/write.js
@@ -2,10 +2,21 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (e) => {
     e.preventDefault();
-    const res = await fetch("/item", {
-        method: "POST",
-        body: new FormData(form),
-    })
+
+    const body = new FormData(form);
+    body.append("insertAt", new Date().getTime());
+
+    try {
+        const res = await fetch("/item", {
+            method: "POST",
+            body: body,
+        });
+
+        const data = await res.json();
+        if (data == "200") window.location.pathname = "/";
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 form.addEventListener("submit", handleSubmitForm);
