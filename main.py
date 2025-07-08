b56fa662ee511af6021eecf45309a9a49cf2c42c
minjeong46 <eyou46@naver.com>
2025-07-08 18:57:10 +0900

    feat: 회원가입 로직 구현

--- main.py
+++ main.py
@@ -71,6 +71,12 @@ def read_image(item_id):
     image_bytes = cur.execute(f"""
                               SELECT image FROM items WHERE id={item_id}
                               """).fetchone()[0]
-    return Response(content=bytes.fromhex(image_bytes))
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
+
+@app.post("/signup")
+def signup(id:Annotated[str,Form()], password:Annotated[str,Form()]):
+    print(id, password)
+    return '200'
+
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
